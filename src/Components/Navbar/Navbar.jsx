e38b2f8a4e09db42a86edac1860bf13d05258f58
dev-imran1 <freealltime247@gmail.com>
2024-08-05 02:25:22 +0600

    navbar 768 desing done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,20 +5,28 @@ import { FaArrowPointer } from "react-icons/fa6";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { FaAlignJustify } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
-
+import { useRef } from 'react';
 
 
 const Navbar = () => {
+  const menuRef = useRef();
   const [menu, setMenu] = useState("home");
+
+  const openMenu = ()=>{
+    menuRef.current.style.right="0";
+  }
+  const closeMenu = ()=>{
+    menuRef.current.style.right="-350px";
+  }
 
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="logo" />
-        <FaAlignJustify className="nav-open__iocn"/>
+        <FaAlignJustify onClick={openMenu} className="nav-open__iocn"/>
       </div>
-      <ul className="nav-menu">
-        <IoClose className="nav-close__iocn"/>
+      <ul ref={menuRef} className="nav-menu">
+        <IoClose className="nav-close__iocn" onClick={closeMenu}/>
         <li><AnchorLink className="anchor-link" offset={50} href="#home"> <p onClick={() => setMenu("home")}>Home</p></AnchorLink>{menu === "home" ? <FaArrowPointer /> : <></>}</li>
         <li><AnchorLink className="anchor-link" offset={50} href="#about"> <p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <FaArrowPointer /> : <></>}</li>
         <li><AnchorLink className="anchor-link" offset={50} href="#service"><p onClick={() => setMenu("service")}>Service</p></AnchorLink>{menu === "service" ? <FaArrowPointer /> : <></>}</li>
