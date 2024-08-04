023a1566e8f9d9cd9d0b4e419cbb29b9e870736d
dev-imran1 <freealltime247@gmail.com>
2024-08-05 03:48:24 +0600

    work part

--- src/Components/Mywork/MyWork.jsx
+++ src/Components/Mywork/MyWork.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './mywork.css';
-import arrow from '../../assets/arrow.svg';
-import mywork_data from '../../assets/mywork_data.js';
-
+import React from "react";
+import "./mywork.css";
+import arrow from "../../assets/arrow.svg";
+import mywork_data from "../../assets/mywork_data.js";
 
 const MyWork = () => {
   return (
-    <div id='work' className='work'>
+    <div id="work" className="work">
       <div className="mywork-title">
         <h1>My Last Work</h1>
         {/* <img src={arrow} alt="" /> */}
       </div>
       <div className="mywork-container">
-        {mywork_data.map((work, index)=>(
-                <img key={index} src={work.w_img} alt="" />
+        {mywork_data.map((work, index) => (
+          <div className="work-image" key={index}> <img src={work.w_img} alt="" /> </div>
+         
         ))}
       </div>
       <div className="mywork-shomore">
@@ -21,7 +21,7 @@ const MyWork = () => {
         <img src={arrow} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyWork
+export default MyWork;
